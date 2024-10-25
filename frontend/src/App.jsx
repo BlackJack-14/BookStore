2683f25272eb5bf764bfb2752990d615de9c4ab0
Rudra gupta <rudragupta0123@gmail.com>
2024-10-25 17:30:22 +0530

    frontend - added home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import { Routes, Route, useParams, Navigate } from "react-router-dom";
+import { useState } from "react";
+import HomePage from "./pages/homePage.jsx";
+import NavBar from "./Components/NavBar/NavBar.jsx";
+import LoginPage from "./pages/loginPage/loginPage.jsx";
+import CreateUserPage from "./pages/createUsePage/createUserPage.jsx";
+import ForgotPasswordPage from "./pages/forgotPasswordPage/forgotPasswordPage.jsx";
+import AdminHomePage from "./pages/adminHomePage/adminHomePage.jsx";
+import EditBookPage from "./pages/editBookPage/editBookPage.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isLoggedin, setisLoggedin] = useState(true);
+  const [isAdmin, setisAdmin] = useState(true);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar login={isLoggedin} setLogin={setisLoggedin} />
+              {isAdmin && isLoggedin ? <AdminHomePage /> : <HomePage />}
+            </>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            isAdmin && isLoggedin ? (
+              <>
+                <NavBar login={isLoggedin} setLogin={setisLoggedin} />
+                <EditBookPage />
+              </>
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            <>
+              <NavBar login={"hide"} />
+              <LoginPage login={setisLoggedin} admin={setisAdmin} />
+            </>
+          }
+        />
+        <Route
+          path="/SignUp"
+          element={
+            <>
+              <NavBar login={isLoggedin} />
+              <CreateUserPage />
+            </>
+          }
+        />
+        <Route
+          path="/ForgotPassword"
+          element={
+            <>
+              <NavBar login={isLoggedin} />
+              <ForgotPasswordPage />
+            </>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
